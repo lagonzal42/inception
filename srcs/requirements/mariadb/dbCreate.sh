77171bd9184a979cacf82102be03365c017bfd65
larra <larra@Ubuntu.lagonzal.42.fr>
2024-04-16 13:41:24 +0200

    Wordpress funciona con nginx pero no son capaces de encontrar la base de datos

--- srcs/requirements/mariadb/dbCreate.sh
+++ srcs/requirements/mariadb/dbCreate.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
 #db_name = Database Name
 #db_user = User
 #db_pwd = User Password
+
+mkdir -p var/run/mysqld
+chown -R mysql:mysql /var/run/mysqld
+chmod 777 /var/run/mysqld
 
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' ;" >> db1.sql
@@ -9,3 +15,6 @@ echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 mysql < db1.sql
+
+service mysqld restart
+mysqld
